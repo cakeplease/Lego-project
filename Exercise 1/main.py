8b64984b51d65e402feb6e49e161341d4c41feb1
Kasia <kasia@sircon.no>
2021-09-22 22:27:34 +0200

    Uploading first exercise

---
+++ Exercise 1/main.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env pybricks-micropython
+from pybricks.hubs import EV3Brick
+from pybricks.ev3devices import Motor
+from pybricks.parameters import Port
+from pybricks.robotics import DriveBase
+from pybricks.tools import wait
+
+# Initialize the EV3 brick.
+ev3 = EV3Brick()
+# Initialize a motor at port B.
+left_motor = Motor(Port.B)
+right_motor = Motor(Port.C)
+
+robot = DriveBase(left_motor, right_motor, wheel_diameter=56, axle_track=115)
+
+ev3.screen.print("Hello world.")
+wait(5000)
+
+robot.straight(200)
+robot.turn(90)
+
+robot.straight(400)
+robot.turn(90)
+
+robot.straight(200)
+robot.turn(90)
+
+robot.straight(400)
+robot.turn(90)
+
+ev3.speaker.say("Have a nice day")
